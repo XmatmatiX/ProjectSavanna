c63a77da289c153ab932d45895786a32253c0df8
Mateusz Boroch <m.boroch2@gmail.com>
2022-05-30 22:27:52 +0200

    Add unit tests

---
+++ ProjectSavanna.Test/AnimalTest.cs
@@ -0,0 +1,18 @@
+﻿using ProjectSavanna;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProjectSavanna.Test
+{
+    public class AnimalTest
+    {
+        [Fact]
+        public void GetPosition()
+        {
+        }
+    }
+}
---
+++ ProjectSavanna.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+
+using System;
+using Xunit;
+
+namespace ProjectSavanna.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
